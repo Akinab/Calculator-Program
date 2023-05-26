d2848a17bc5ffa79053e6d17568978a1313c503e
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-26 17:45:37 +0800

    Defining the details of the program.

---
+++ calc prog.py
@@ -0,0 +1,6 @@
+def details ():
+    Description = "Simple App Calculator using OOP concept."
+    Date = "05-27-23"
+    print ("\nDescription: {}\nDate: {}".format (Description , Date))
+    
+details ()
