05f24f6c1326efd1e20b5a6b9b5e04c1e78f1592
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-26 18:00:12 +0800

    if details

--- calc prog.py
+++ calc prog.py
@@ -54,9 +54,12 @@ def main():
                 print("\nResult of Division:", result)
         else:
             print("\nInvalid choice. Please try again.")
-            
+
         answer = input("Do you want to try again? (y/n): ")
         if answer.lower() != 'y':
             print("\n\nThank you for using this program!")
             break
         print()
+
+if __name__ == '__main__':
+    main()
